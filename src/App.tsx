904c9512e5acc5b867bfd0a76affec3b3fda849f
Aaron <aaronmc52@hotmail.com>
2026-05-12 11:35:14 -0400

    feat: wire 8-section marketing site layout (ProductShowcase deferred to post-app-rebrand)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './index.css'
 import { Hero } from './components/Hero'
+import { ConnectsTo } from './components/ConnectsTo'
 import { PainPoints } from './components/PainPoints'
+import { HowItWorks } from './components/HowItWorks'
 import { Features } from './components/Features'
+import { SocialProof } from './components/SocialProof'
 import { OwnerProfiles } from './components/OwnerProfiles'
 import { WaitlistCTA } from './components/WaitlistCTA'
 import { Footer } from './components/Footer'
@@ -10,8 +13,11 @@ function App() {
   return (
     <div className="min-h-screen bg-[#0a0f1e] text-white">
       <Hero />
+      <ConnectsTo />
       <PainPoints />
+      <HowItWorks />
       <Features />
+      <SocialProof />
       <OwnerProfiles />
       <WaitlistCTA />
       <Footer />
